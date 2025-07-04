069f20b867225c41038152159532dd5899133aae
Raushankumar1 <you@example.com>
2025-07-04 12:47:41 +0530

    added  checkAuth for better route security for client

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,6 +3,7 @@ const response = require("../utils/response");
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const generateToken = require("../utils/generateToken");
+const jwt = require("jsonwebtoken");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -70,4 +71,18 @@ const logoutUser = asyncHandler(async (req, res) => {
   return response.success(res, "Logout successful");
 });
 
-module.exports = { registerUser, loginUser, logoutUser };
+const checkAuth = (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ authenticated: false });
+  }
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    return res.status(200).json({ authenticated: true, user: payload });
+  } catch (err) {
+    console.error("JWT Verification Error:", err.message);
+    return res.status(401).json({ authenticated: false });
+  }
+};
+
+module.exports = { registerUser, loginUser, logoutUser, checkAuth };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,6 +3,7 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  checkAuth,
 } = require("../controllers/auth.controller");
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.route("/regitser").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
+router.route("/check").get(checkAuth);
 
 module.exports = router;
