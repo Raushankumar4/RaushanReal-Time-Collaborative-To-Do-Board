08ce28e4a5caecdb42c5c2a7cd03c8efca787138
Raushankumar1 <you@example.com>
2025-07-04 10:46:12 +0530

    Added Socket.IO for real-time task updates and event broadcasting

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,5 +1,29 @@
 module.exports = function handleSocketConnection(io) {
   io.on("connection", (socket) => {
-    console.log(`Connected ${socket.id}`);
+    console.log("User connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+
+    // Task updated → broadcast to others
+    socket.on("task:updated", (data) => {
+      socket.broadcast.emit("task:updated", data);
+    });
+
+    // Task created
+    socket.on("task:created", (data) => {
+      socket.broadcast.emit("task:created", data);
+    });
+
+    // Task deleted
+    socket.on("task:deleted", (data) => {
+      socket.broadcast.emit("task:deleted", data);
+    });
+
+    // Log updated (optional)
+    socket.on("log:added", (data) => {
+      socket.broadcast.emit("log:added", data);
+    });
   });
 };
