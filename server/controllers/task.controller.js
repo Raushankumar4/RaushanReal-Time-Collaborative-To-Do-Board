30555d31576f2f7e87e6ca28886068f466998813
Raushankumar1 <you@example.com>
2025-07-04 10:25:02 +0530

    Added GetTasks UpdateTask DeleteTask

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -28,4 +28,47 @@ const createTask = asyncHandler(async (req, res) => {
   return response.success(res, "Task Created!", task);
 });
 
-module.exports = { createTask };
+const getTasks = asyncHandler(async (_, res) => {
+  const tasks = await Task.find().populate("assignedTo", "name email");
+  return response.success(res, "Tasks", tasks);
+});
+
+const updateTask = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const task = await Task.findById(id);
+
+  if (!task) {
+    return response.error(res, "Task not found!", 404);
+  }
+
+  if (new Date(req.body.lastEditedAt) < task.lastEditedAt) {
+    return response.error(res, "Conflict detected", 409);
+  }
+
+  Object.assign(task, req.body, { lastEditedAt: Date.now() });
+  await task.save();
+
+  await Action.create({
+    actionType: "updated",
+    task: task._id,
+    user: req.user.id,
+  });
+
+  return response.success(res, "Task updated!", task);
+});
+
+const deleteTask = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const task = await Task.findById(id);
+  if (!task) return response.error(res, "Task Not Found!", 404);
+
+  await Action.create({
+    actionType: "deleted",
+    task: task._id,
+    user: req.user.id,
+  });
+  await task.deleteOne();
+  return response.success(res, "Task Deleted!");
+});
+
+module.exports = { createTask, getTasks, updateTask, deleteTask };
--- server/routes/task.route.js
+++ server/routes/task.route.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
-const { createTask } = require("../controllers/task.controller");
+const {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+} = require("../controllers/task.controller");
 const isAuthenticated = require("../middleware/isAuthenticated");
-
 
 const router = Router();
 
-router.route("/create").post(isAuthenticated,createTask);
+router.route("/create").post(isAuthenticated, createTask);
+router.route("/").get(isAuthenticated, getTasks);
+router.route("/update/:id").put(isAuthenticated, updateTask);
+router.route("/delete/:id").delete(isAuthenticated, deleteTask);
 
 module.exports = router;
